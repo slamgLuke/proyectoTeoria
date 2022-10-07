5dad73adbf62fe525d5b376fa6e2e20e2d4daf8f
slamgLuke <46696548+slamgLuke@users.noreply.github.com>
2022-10-07 17:01:44 -0500

    primer comi

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+
+/*
+Integrantes:
+Lucas Carranza 202210073
+PONER NOMBRE y cod CARAJO00000
+
+Ejercicio de formula 1 (el mas facil)
+*/
+
+using namespace std;
+
+int timelapse() {
+  float tiempo;
+  tiempo = float(rand()%10) / 10;
+  return tiempo;
+}
+
+int main() {
+  srand(time(0));
+  
+  // MAIN LOOP
+  
+  return 0;
+}
