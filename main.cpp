a1e5ac986f3509ce77187daace10cb2b9cb2879a
slamgLuke <46696548+slamgLuke@users.noreply.github.com>
2022-10-10 12:53:00 -0500

    comitn 4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+
 using namespace std;
 
+
 bool verify(float *recorrido, int index, vector<int> &pasos){
-    float x = float(rand()%10) / 10;
+    float x = float(rand()%100) / 100.0;
 
     if (x > 0.8) {
         *recorrido += 83;}
@@ -17,7 +19,7 @@ bool verify(float *recorrido, int index, vector<int> &pasos){
     else {
         *recorrido -= 10;}
 
-    if (*recorrido >= 300000) {
+    if (*recorrido >= 100) {
         return true;
     }
     return false;
@@ -32,19 +34,30 @@ void ganadores(vector<int> llegaron, vector<int> pasos) {
 }
 
 
- void desplazamiento(vector<float> &recorridos, vector<int> &pasos){
+ void desplazamiento(vector<float> &recorridos){
     // variables
     vector<int> llegaron;    
     vector<int> siguen = {0, 1, 2, 3, 4, 5, 6, 7};
+    vector<int> pasos = {0, 0, 0, 0, 0, 0, 0, 0};
 
     // Lista del orden de llegada. Cuando todos llegan el programa termina
-    while(llegaron.size() < 8) { 
-        for (int i : siguen) {
-            if (verify(&(recorridos[i]), i, pasos)) {
-                siguen.erase(siguen.begin()+i);
-                llegaron.push_back(i);
+    while(siguen.size() >= 1) { 
+        for (auto i = siguen.begin(); i != siguen.end(); i++) {
+            cout << *i << endl;
+            pasos[*i]++;
+            if (verify(&(recorridos[*i]), *i, pasos)) {
+                cout << "pasos de " << *i  << ": " << pasos[*i] << endl;
+                cout << "Llego " << *i <<  endl;
+                
+                siguen.erase(i);
+                llegaron.push_back(*i);
+                i--;
+
+                for (int j=0; j<siguen.size(); j++) {
+                    cout << siguen[j] << " ";
+                }
+                cout << endl;
             }
-            pasos[i]++;
         }
     }
     // Fin
@@ -56,10 +69,10 @@ int main(){
     srand(time(NULL));
     // 8 autos, 8 carriles de 300K(300 000m), gana el que llegue a 300km en la menor cantidad de pasos.
     vector<float> recorridos = {0,0,0,0,0,0,0,0};
-    vector<int> pasos = {0,0,0,0,0,0,0,0};
+    
 
     // main loop
-    desplazamiento(recorridos, pasos);
+    desplazamiento(recorridos);
 
     cout << "listo";
 
