fb5c26a9a42d42b25295679e6ce14f88a8fa49fb
slamgLuke <46696548+slamgLuke@users.noreply.github.com>
2022-10-10 14:52:36 -0500

    listo
    :)

--- main.cpp
+++ main.cpp
@@ -5,76 +5,85 @@
 using namespace std;
 
 
-bool verify(float *recorrido, int index, vector<int> &pasos){
+int menosPasos(vector<int> pasos) {
+    int menor = 0;
+    for (int i=0; i<pasos.size(); i++) {
+        if (pasos[i] < pasos[menor])
+        menor = i;
+    }
+    return menor;
+}
+
+
+bool verify(int *recorrido, int index, vector<int> &pasos){
     float x = float(rand()%100) / 100.0;
 
-    if (x > 0.8) {
-        *recorrido += 83;}
-    else if (x <= 0.8 && x > 0.6) {
-        *recorrido += 50;}
-    else if (x <= 0.6 && x > 0.4) {
-        *recorrido += 30;}
-    else if (x <= 0.4 && x > 0.2) {
-        pasos[index] += 2;}
-    else {
-        *recorrido -= 10;}
+    if (x > 0.8)
+        *recorrido += 83;
+    else if (x <= 0.8 && x > 0.6)
+        *recorrido += 50;
+    else if (x <= 0.6 && x > 0.4)
+        *recorrido += 30;
+    else if (x <= 0.4 && x > 0.2)
+        pasos[index] += 2;
+    else 
+        *recorrido -= 10;
 
-    if (*recorrido >= 100) {
+    if (*recorrido >= 300000)
         return true;
-    }
     return false;
 }
 
 
-void ganadores(vector<int> llegaron, vector<int> pasos) {
-    string nombres[8] = {"Toreto", "RayoMc", "Mate", "GhostRider", "BatMovil", "MysteryMachine", "CJsBike", "LaChama"};
-    for (int i=0; i<8; i++) {
-        cout << "Llego en " << i << " puesto: " << nombres[llegaron[i]] << endl;
-    }
+void ganadores(vector<int> llegaron, vector<int> pasos, vector<string> nombres) {
+    // obteniendo el primer lugar
+    int primero = menosPasos(pasos);
+    cout << "Ganador: " << nombres[primero] << ", cantidad de pasos: " << pasos[primero] << " pasos" << endl;
+    pasos.erase(pasos.begin()+primero);
+    nombres.erase(nombres.begin()+primero);
+
+    // obteniendo el segundo lugar
+    int segundo = menosPasos(pasos);
+    cout << "2do puesto: " << nombres[segundo] << ", cantidad de pasos: " << pasos[segundo] << " pasos" << endl;    
+    pasos.erase(pasos.begin()+segundo);
+    nombres.erase(nombres.begin()+segundo);
+
+    // obteniendo el tercer lugar
+    int tercero = menosPasos(pasos);
+    cout << "3er puesto: " << nombres[tercero] << ", cantidad de pasos: " << pasos[tercero] << " pasos" << endl;
 }
 
 
- void desplazamiento(vector<float> &recorridos){
-    // variables
-    vector<int> llegaron;    
+ void desplazamiento(vector<int> &recorridos, vector<int> &pasos, vector <int> &llegaron){
+    // vectores
     vector<int> siguen = {0, 1, 2, 3, 4, 5, 6, 7};
-    vector<int> pasos = {0, 0, 0, 0, 0, 0, 0, 0};
 
     // Lista del orden de llegada. Cuando todos llegan el programa termina
-    while(siguen.size() >= 1) { 
+    while(llegaron.size() < 8) { 
         for (auto i = siguen.begin(); i != siguen.end(); i++) {
-            cout << *i << endl;
             pasos[*i]++;
             if (verify(&(recorridos[*i]), *i, pasos)) {
-                cout << "pasos de " << *i  << ": " << pasos[*i] << endl;
-                cout << "Llego " << *i <<  endl;
-                
-                siguen.erase(i);
                 llegaron.push_back(*i);
+                siguen.erase(i);
                 i--;
-
-                for (int j=0; j<siguen.size(); j++) {
-                    cout << siguen[j] << " ";
-                }
-                cout << endl;
             }
         }
     }
-    // Fin
-    ganadores(llegaron, pasos);
 }
 
 
 int main(){
     srand(time(NULL));
     // 8 autos, 8 carriles de 300K(300 000m), gana el que llegue a 300km en la menor cantidad de pasos.
-    vector<float> recorridos = {0,0,0,0,0,0,0,0};
+    vector<string> nombres = {"RayoMcQueen", "Mate", "Francesco", "Strip Weathers", "Chick Hicks", "Jackson Storm", "Guido"};
+    vector<int> recorridos(8, 0);
+    vector<int> pasos(8, 0);
+    vector<int> llegaron;
     
-
     // main loop
-    desplazamiento(recorridos);
+    desplazamiento(recorridos, pasos, llegaron);
 
-    cout << "listo";
-
+    // final de la carrera
+    ganadores(llegaron, pasos, nombres);
     return 0;
 }
